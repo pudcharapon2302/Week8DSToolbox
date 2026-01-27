bbc08adae0020b6fce4d2253a06645df4b6647d9
Pansak <pansak.pa.66@ubu.ac.th>
2026-01-27 11:12:48 +0700

    commit

---
+++ pycaretflow.py
@@ -0,0 +1,11 @@
+from pycaret.datasets import get_data
+from pycaret.classification import *
+
+data = get_data('diabetes')
+#print(data.head())
+
+exp = ClassificationExperiment()
+exp.setup(data, target='Class variable', session_id=123)
+
+best_model = exp.compare_models()
+print(best_model)
